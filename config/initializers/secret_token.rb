a8a1e93e0e77a14abbea3e593f43ceae3fe35c5b
plexcode <sking@plexcode.com>
2012-03-12 22:24:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f17fee385fd86023fc4071c5ca509a013cb9c97279d30c0ba52e3c9964bb6552bc2bfe4920ed0bfcba1de72dec8f9c1802f2edcdadcbcbd41e5fb41985458d70'
